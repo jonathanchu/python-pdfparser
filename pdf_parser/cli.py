7e8142e75b011360b61b0a233dfc2b8a000fedf2
Jonathan Chu <jonathan.chu@me.com>
2014-12-02 22:09:55 -0500

    First attempt at processing a sample pdf to stdout.

--- pdf_parser/cli.py
+++ pdf_parser/cli.py
@@ -1,7 +1,26 @@
+import io
+import sys
+
 import click
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
 
 
 @click.command()
 def main():
     """Parses PDFs."""
-    click.echo('hello')
+    fname = '/Users/jonathan/Desktop/simple1.pdf'
+    caching = True
+    maxpages = 0
+    password = ''
+    pagenos = set()
+    rsrcmgr = PDFResourceManager(caching=caching)
+    outfp = sys.stdout
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, outfp, laparams=laparams)
+    fp = io.open(fname, 'rb')
+    process_pdf(rsrcmgr, device, fp, pagenos, maxpages=maxpages,
+                password=password, caching=caching, check_extractable=True)
+    fp.close()
+    # click.echo('hello')
